8856f4ce92568952fafc53349ef72212d203d099
mrmrprogrammer <mr.mr.programmer@gmail.com>
2025-11-18 08:00:58 +0330

    Finish solving the second question

--- practice_01/questation_02/code_01.py
+++ practice_01/questation_02/code_01.py
@@ -1,15 +1,9 @@
 import cv2
 import numpy as np
 
-# ---------------------------
-# خواندن تصویر
-# ---------------------------
 img = cv2.imread("image.png")
 img_display = img.copy()
 
-# ---------------------------
-# پنجره کنترل
-# ---------------------------
 def nothing(x):
     pass
 
@@ -19,38 +13,28 @@ cv2.createTrackbar("Red",    "Control Panel", 255, 255, nothing)
 cv2.createTrackbar("Green",  "Control Panel", 255, 255, nothing)
 cv2.createTrackbar("Blue",   "Control Panel", 255, 255, nothing)
 
-# ---------------------------
-# تابع کلیک ماوس
-# ---------------------------
 def draw_circle(event, x, y, flags, param):
     global img_display
 
     if event == cv2.EVENT_LBUTTONDOWN:
         radius = cv2.getTrackbarPos("Radius", "Control Panel")
 
-        # رنگ انتخاب شده در بخش (الف)
         b = cv2.getTrackbarPos("Blue", "Control Panel")
         g = cv2.getTrackbarPos("Green", "Control Panel")
         r = cv2.getTrackbarPos("Red", "Control Panel")
 
         color = (b, g, r)
 
-        # رسم دایره روی تصویر
         cv2.circle(img_display, (x, y), radius, color, -1)
 
-# ثبت تابع کلیک
 cv2.namedWindow("Main")
 cv2.setMouseCallback("Main", draw_circle)
 
-
-# ---------------------------
-# حلقه نمایش
-# ---------------------------
 while True:
     cv2.imshow("Main", img_display)
 
     key = cv2.waitKey(1) & 0xFF
-    if key == 27:   # ESC
+    if key == 27:
         break
 
 cv2.destroyAllWindows()
